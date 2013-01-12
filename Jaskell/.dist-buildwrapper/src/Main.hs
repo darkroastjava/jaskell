57cc09c374b152347be67cfa8ddf918184fc3098
darkroastjava <danieljaegerjordi@gmail.com>
2013-01-12 23:42:07 +0100

    Added initial files

---
+++ Jaskell/.dist-buildwrapper/src/Main.hs
@@ -0,0 +1,19 @@
+--module Main where
+
+
+
+--main::IO()
+--main = undefined
+
+
+rep = \ x -> x:rep x
+
+nimm1 = \ (x:xs) -> x
+
+--nimm = \ n (x:xs) -> if n==0 then [] else x:(nimm (n-1) xs)
+nimm 0 xs = []
+nimm n (x:xs) = x:(nimm (n-1) xs)
+--nimm n [] = error "leere liste"
+nimm n [] = []
+
+r
