bd12db4ee3a4958afa4d676b5e8da2f4cdf0e8d6
darkroastjava <danieljaegerjordi@gmail.com>
2013-01-13 00:36:58 +0100

    Defined MiniProgram and implemented eval App2 Plus ...

--- Jaskell/src/Main.hs
+++ Jaskell/src/Main.hs
@@ -1,18 +1,17 @@
---module Main where
+module Main where
+
+-- +, *, 0.., App
+
+data MiniProgram = Lit Integer
+  | Plus
+  | Mult
+  | App2 MiniProgram MiniProgram MiniProgram 
+  deriving Show
 
 
+input = App2 Plus (Lit 5) (Lit 6)
+input2 = App2 Plus input (Lit 9)
 
---main::IO()
---main = undefined
-
-
-rep = \ x -> x:rep x
-
-nimm1 = \ (x:xs) -> x
-
---nimm = \ n (x:xs) -> if n==0 then [] else x:(nimm (n-1) xs)
-nimm 0 xs = []
-nimm n (x:xs) = x:(nimm (n-1) xs)
---nimm n [] = error "leere liste"
-nimm n [] = []
-
+eval (Lit n) = Lit n
+eval (App2 Plus (Lit n) (Lit m)) = Lit (n+m)
+eval (App2 Plus mp1 mp2) = eval (App2 Plus (eval mp1) (eval mp2))
