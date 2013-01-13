c27fc79311c8e19635d3c613b731b0e96076d291
Christian Jaeger <chrjae@gmail.com>
2013-01-13 06:12:42 -0500

    Mult

--- Jaskell/src/Main.hs
+++ Jaskell/src/Main.hs
@@ -8,10 +8,13 @@ data MiniProgram = Lit Integer
   | App2 MiniProgram MiniProgram MiniProgram 
   deriving Show
 
-
 input = App2 Plus (Lit 5) (Lit 6)
 input2 = App2 Plus input (Lit 9)
 
 eval (Lit n) = Lit n
+
 eval (App2 Plus (Lit n) (Lit m)) = Lit (n+m)
-eval (App2 Plus mp1 mp2) = eval (App2 Plus (eval mp1) (eval mp2))
+eval (App2 Mult (Lit n) (Lit m)) = Lit (n*m)
+
+eval (App2 mpop mp1 mp2) = eval (App2 mpop (eval mp1) (eval mp2))
+
