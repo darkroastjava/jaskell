4d03243b75fc823c892a08c0ca9473b106ed76c1
Christian Jaeger <chrjae@gmail.com>
2013-01-13 06:20:11 -0500

    where

--- Jaskell/src/Main.hs
+++ Jaskell/src/Main.hs
@@ -8,8 +8,8 @@ data MiniProgram = Lit Integer
   | App2 MiniProgram MiniProgram MiniProgram 
   deriving Show
 
-input = App2 Plus (Lit 5) (Lit 6)
-input2 = App2 Plus input (Lit 9)
+--input = App2 Plus (Lit 5) (Lit 6)
+--input2 = App2 Plus input (Lit 9)
 
 eval (Lit n) = Lit n
 
@@ -18,3 +18,5 @@ eval (App2 Mult (Lit n) (Lit m)) = Lit (n*m)
 
 eval (App2 mpop mp1 mp2) = eval (App2 mpop (eval mp1) (eval mp2))
 
+
+x = eval (square (App2 Plus (Lit 1)(Lit 2))) where square x = App2 Mult x x
