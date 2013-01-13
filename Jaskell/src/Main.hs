ccb02aafcb0603453e0f96a469efa7a262e41c75
Christian Jaeger <chrjae@gmail.com>
2013-01-13 08:54:10 -0500

    (typ error)

--- Jaskell/src/Main.hs
+++ Jaskell/src/Main.hs
@@ -8,6 +8,7 @@ import Data.Char
 data MiniWert = Num Integer
   | Plus
   | Mult
+  | Func Char Char String
   deriving Show
 
 --"2"
@@ -17,25 +18,24 @@ data MiniWert = Num Integer
 --"(P(P13)(M12))"
 --"((P13)(M12)4)"
 --"=v5(Pvv)"
-
-input = "P56"
-
-
---parse ('P':c1:c2:[]) = App2 Plus (parse [c1]) (parse [c2])
-
---parse ('(':cs) = (App2 arg1 arg2 arg3, csfinal)
---  where (arg1,cs') = parse cs
---        (arg2,cs'') = parse cs'
---        (arg3,')':csfinal) = parse cs''
+--"=dFpq{(Mp2)}(d30)" ~> Num 6
 
 call Plus (Num m) (Num n) = Num (m+n)
 call Mult (Num m) (Num n) = Num (m*n)
+call (Func p q prog) x y = parseval prog ((p,x):(q,y):[])
 
 assoc key [] = error "Key not found in context"
 
 assoc key ((var,val):ctx) = 
   if (key == var) then val else assoc key ctx
 
+parsecurly ('}':cs) = ("", cs)
+
+parsecurly (c:cs) = (c:prog, csfinal)
+  where (prog, csfinal) = parsecurly cs
+
+parseval ('F':p:q:'{':cs) ctx = (Func p q prog, csfinal)
+  where (prog,csfinal) = parsecurly cs
 
 parseval ('(':cs) ctx = (call arg1 arg2 arg3, csfinal)
   where (arg1,cs') = parseval cs ctx
