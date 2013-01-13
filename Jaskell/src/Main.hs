756a1c33115ec728ea0af6be44a1fb9157b77943
Christian Jaeger <chrjae@gmail.com>
2013-01-13 07:42:43 -0500

    parseeval implementiert
    parseval "(P(M23)4)"
    (Num 10,"")

--- Jaskell/src/Main.hs
+++ Jaskell/src/Main.hs
@@ -5,10 +5,9 @@ import Data.Char
  
 -- +, *, 0.., App
 
-data MiniProgram = Lit Integer
+data MiniWert = Num Integer
   | Plus
   | Mult
-  | App2 MiniProgram MiniProgram MiniProgram 
   deriving Show
 
 --"2"
@@ -24,29 +23,24 @@ input = "P56"
 
 --parse ('P':c1:c2:[]) = App2 Plus (parse [c1]) (parse [c2])
 
-parse ('(':cs) = (App2 arg1 arg2 arg3, csfinal)
-  where (arg1,cs') = parse cs
-        (arg2,cs'') = parse cs'
-        (arg3,')':csfinal) = parse cs''
-        
-parse ('P':cs) = (Plus, cs)
-parse ('M':cs) = (Mult, cs)
+--parse ('(':cs) = (App2 arg1 arg2 arg3, csfinal)
+--  where (arg1,cs') = parse cs
+--        (arg2,cs'') = parse cs'
+--        (arg3,')':csfinal) = parse cs''
 
-parse (c:cs) = 
+call Plus (Num m) (Num n) = Num (m+n)
+call Mult (Num m) (Num n) = Num (m*n)
+
+parseval ('(':cs) = (call arg1 arg2 arg3, csfinal)
+  where (arg1,cs') = parseval cs
+        (arg2,cs'') = parseval cs'
+        (arg3,')':csfinal) = parseval cs''
+        
+parseval ('P':cs) = (Plus, cs)
+parseval ('M':cs) = (Mult, cs)
+
+parseval (c:cs) = 
   if (c <= '9')&&(c >= '0') then
-    (Lit (toInteger ((ord c) - (ord '0'))), cs)
+    (Num (toInteger ((ord c) - (ord '0'))), cs)
   else
     error "unknown"
-
---input = App2 Plus (Lit 5) (Lit 6)
---input2 = App2 Plus input (Lit 9)
-
-eval (Lit n) = Lit n
-
-eval (App2 Plus (Lit n) (Lit m)) = Lit (n+m)
-eval (App2 Mult (Lit n) (Lit m)) = Lit (n*m)
-
-eval (App2 mpop mp1 mp2) = eval (App2 mpop (eval mp1) (eval mp2))
-
-
-x = eval (square (App2 Plus (Lit 1)(Lit 2))) where square x = App2 Mult x x
