3a047ad2368c697693d345fb1e9eef55999bebc4
Christian Jaeger <chrjae@gmail.com>
2013-01-13 06:45:31 -0500

    parse for App2 Plus

--- Jaskell/src/Main.hs
+++ Jaskell/src/Main.hs
@@ -1,5 +1,8 @@
 module Main where
 
+import Data.Char
+
+ 
 -- +, *, 0.., App
 
 data MiniProgram = Lit Integer
@@ -7,6 +10,18 @@ data MiniProgram = Lit Integer
   | Mult
   | App2 MiniProgram MiniProgram MiniProgram 
   deriving Show
+
+--"PP13M12"
+
+input = "P56"
+
+parse ('P':c1:c2:[]) = App2 Plus (parse [c1]) (parse [c2])
+
+parse [c] = 
+  if (c <= '9')&&(c >= '0') then
+    Lit (toInteger ((ord c) - (ord '0')))
+  else
+    error "unknown"
 
 --input = App2 Plus (Lit 5) (Lit 6)
 --input2 = App2 Plus input (Lit 9)
